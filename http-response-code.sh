3d15fd0d271ca139e18d5997ce3c4f1f1df4e938
Jonathan Villa <jonathan@1030tech.com>
2014-09-07 23:37:37 +0000

    troubleshooting

--- http-response-code.sh
+++ http-response-code.sh
@@ -3,6 +3,7 @@
 # Author: Jonathan Villa <jonthan@1030tech.com>
 # Description: Makes a simple HTTP request to a provided URL using curl. This is beneficial when monitoring for 500/404/403/etc responses.
 URL=`grep url ./param.json | awk '{ print $2 }' | sed 's/"//g' | sed 's/,//g'`
+SOURCE=`grep source ./param.json | awk '{ print $2 }' | sed 's/"//g' | sed 's/,//g'`
 
 while true; do
 	if [ "$URL" == "" ]; then
@@ -10,7 +11,7 @@ while true; do
 		exit 1
 	else
 		HTTP_RESPONSE_CODE=`curl -s -w "%{http_code}\n" -o ./http_code.txt $URL`
-		echo "HTTP_RESPONSE_CODE $HTTP_RESPONSE_CODE"
+		echo "HTTP_RESPONSE_CODE $HTTP_RESPONSE_CODE $SOURCE"
 		#sleep 1
 	fi
 done
