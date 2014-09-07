49d1a2dbf72c66513da0713c55b59cf9d6ccd799
Jonathan Villa <jonathan@1030tech.com>
2014-09-07 00:43:33 +0000

    adding

--- http-response-code.sh
+++ http-response-code.sh
@@ -2,13 +2,16 @@
 #
 # Author: Jonathan Villa <jonthan@1030tech.com>
 # Description: Makes a simple HTTP request to a provided URL using curl. This is beneficial when monitoring for 500/404/403/etc responses.
-RESPONSE=
-URL=$1
+URL=`grep url ./param.json | awk '{ print $2 }' | sed 's/"//g' | sed 's/,//g'`
+INTERVAL=`grep pollInterval ./param.json | awk '{ print $2 }'`
 
-if [ "$URL" == "" ]; then
-	echo "URL not provided";
-	exit 1
-else
-	HTTP_RESPONSE_STATUS=`curl -s -w "%{http_code}\n" -o /tmp/http_status.txt $URL`
-	echo "HTTP_RESPONSE_STATUS $HTTP_RESPONSE_STATUS"
-fi
+while true; do
+	if [ "$URL" == "" ]; then
+		echo "URL not provided";
+		exit 1
+	else
+		HTTP_RESPONSE_CODE=`curl -s -w "%{http_code}\n" -o ./http_code.txt $URL`
+		echo "HTTP_RESPONSE_CODE $HTTP_RESPONSE_CODE"
+		sleep $INTERVAL
+	fi
+done
